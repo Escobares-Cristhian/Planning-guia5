35098100f3c0ec00cb7c20aa3cb0e270b2b69fe1
Escobares-Cristhian Notebook CONAE 027149 <cristhian.escobares@ig.edu.ar>
2024-05-14 11:31:09 -0300

    Completado ejercicio 4 + Corección ejercicio 3

--- eje4.py
+++ eje4.py
@@ -1,15 +1,18 @@
 # !/usr/bin python
 import sys
 
-# ----------------------------------------------------------------------
-# ------------------------- Colocar parámetros -------------------------
-sueldo = 1200
-rango = 3
+# ----- Checkeo de argumentos -----
+if len(sys.argv) != 3:
+    print('Error en la cantidad de argumentos. Se esperaban 2.')
+    print('Uso: python eje3.py <sueldo> <rango>')
+    print('<sueldo>: sueldo bruto del empleado. Debe ser un número real.')
+    print('<rango>: rango del empleado. Debe ser un número entero entre 1 y 3.')
+    sys.exit(1)
 
+# ----- Importo parámetros -----
+sueldo = float(sys.argv[1])
+rango = int(sys.argv[2])
 
-# ----------------------------------------------------------------------
-# ----------------- NO MODIFICAR A PARTIR DE ESTE PUNTO ----------------
-# ----------------------------------------------------------------------
 # ----- Validación de argumentos -----
 if rango not in [1, 2, 3]:
     print('El rango debe ser un número entero entre 1 y 3.')
